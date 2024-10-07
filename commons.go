f40657e82a3251a157675ce617fc95db1301be81
Luciano Silva <lurodrisilva@hotmail.com>
2024-10-07 08:29:10 -0300

    first commit

---
+++ commons.go
@@ -0,0 +1,34 @@
+package commons
+
+import (
+	"io/ioutil"
+	"os"
+	"sort"
+	"strings"
+)
+
+// List files by pattern and creation time
+func listFilesByPatternAndCreationTime(directoryPath, searchPattern string) ([]os.FileInfo, error) {
+
+	// Read the directory
+	files, err := ioutil.ReadDir(directoryPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Filter files by search pattern
+	var matchingFiles []os.FileInfo
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), searchPattern) {
+			matchingFiles = append(matchingFiles, file)
+		}
+	}
+
+	// Sort files by creation time in descending order
+	sort.Slice(matchingFiles, func(i, j int) bool {
+		return matchingFiles[i].ModTime().After(matchingFiles[j].ModTime())
+	})
+
+	// Return the matching files
+	return matchingFiles, nil
+}
